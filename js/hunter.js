aac30442adb5918c0712d06f40bbe8211ddd0af1
Rihanul Islam <rihanthedev@gmail.com>
2023-02-26 02:04:07 +0600

    secound commit

---
+++ js/hunter.js
@@ -0,0 +1,9 @@
+const loadPhone =async ()=>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.data);
+}
+const displayPhone = ()
+
+loadPhone()
