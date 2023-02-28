14ec23b8ad642b6b55b0a2b44968572761c8d57b
Rihanul Islam <rihanthedev@gmail.com>
2023-02-28 20:10:15 +0600

    diffearnce

---
+++ js/functionDifferent.js
@@ -0,0 +1,9 @@
+// what is the differnt of Arrow function vs regular function.?
+// regular function
+
+const hi = function regular(){
+    console.log('Tomr bornona dicche j ami, tomi bujte parteso nah j ');
+}
+
+// here is arrow function
+const a = ()=>console.log('hi');
